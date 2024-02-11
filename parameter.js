40b0e7e75496b62dcc10522e16984b4c2fe6f362
meheraz1100 <meheraz1100@gmail.com>
2024-02-11 23:31:42 +0600

    parameter added

---
+++ parameter.js
@@ -0,0 +1,34 @@
+function vatKhao(){
+    console.log('hand wash kore asho');
+    console.log('boso');
+    console.log('plate e khabar nao');
+    console.log('aste dhire khao');
+}
+
+// vatKhao();
+
+
+// 3 ---->  3*3 = 9
+// 4 ----> 4*4 = 16
+// 11 ----> 11*11 = 121
+
+
+function square(number){
+    console.log(number);
+    console.log('value of number parameter ' , number);
+    const borgo = number * number;
+    console.log('square fo the given number is : ', borgo);
+}
+
+
+// square(4);
+// square(12);
+// square(5);
+// square(405);
+
+function add(num1, num2, num3, num4){
+    const addNums = num1 + num2 + num3 + num4;
+    console.log('the sums of the input values ' , addNums);
+}
+
+add(1,1,1,1);
