0d4ab1aea99fd7a048ca2917f293563950d98387
meheraz1100 <meheraz1100@gmail.com>
2024-02-12 07:58:41 +0600

    arguments return recap conditional

---
+++ arguments.js
@@ -0,0 +1,22 @@
+function doubleIt(number){
+    const doubled = number * 2;
+    console.log(doubled);
+}
+
+console.log('I will call the function');
+doubleIt(15);
+console.log('-------------------');
+doubleIt(88);
+console.log('-------------------');
+doubleIt(873);
+
+
+const money = 55;
+doubleIt(money);
+
+function difference(a, b){
+    const numDiff = a - b;
+    console.log(numDiff);
+}
+
+difference(20, 10)
